8408c1e812951c17a08f70797cdd9cfa44cd0258
seolbotdae <seolbotdae@gmail.com>
2024-05-14 16:53:22 +0900

    Initial Commit

---
+++ PlanIO/PlanIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanIOApp.swift
+//  PlanIO
+//
+//  Created by Seol WooHyeok on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
