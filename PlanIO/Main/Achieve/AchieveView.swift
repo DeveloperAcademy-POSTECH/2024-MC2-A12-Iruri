a09f80ea5c2fe56da58e55ef8817989efee212c7
Anjin Lee <anjin720jin@naver.com>
2024-05-17 14:06:37 +0900

    Feat: 실천 화면 레이아웃

--- PlanIO/Main/Achieve/AchieveView.swift
+++ PlanIO/Main/Achieve/AchieveView.swift
@@ -9,7 +9,66 @@ import SwiftUI
 
 struct AchieveView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            HStack {
+                Text("실천해보자")
+                    .font(.title).bold()
+                
+                Spacer()
+                
+                Image(systemName: "person")
+            }
+            .padding(.horizontal, 40)
+            
+            Divider()
+            
+            HStack(spacing: 24) {
+                // 오늘의 할 일
+                Rectangle()
+                    .frame(width: 360)
+                    .overlay {
+                        Text("오늘의 할 일\n이오")
+                            .foregroundStyle(Color.white)
+                    }
+                
+                VStack(spacing: 24) {
+                    // 전체 공부 현황
+                    Rectangle()
+                        .frame(height: 160)
+                        .overlay {
+                            Text("전체 공부 현황\n다다? 블리시?")
+                                .foregroundStyle(Color.white)
+                        }
+                    
+                    HStack(spacing: 24) {
+                        VStack(spacing: 12) {
+                            // 오늘의 성취
+                            Rectangle()
+                                .overlay {
+                                    Text("오늘의 성취\n다다? 블리시?")
+                                        .foregroundStyle(Color.white)
+                                }
+                            
+                            // 남은 기간
+                            Rectangle()
+                                .overlay {
+                                    Text("남은 기간\n이오")
+                                        .foregroundStyle(Color.white)
+                                }
+                        }
+                        
+                        // 오늘의 한마디
+                        Rectangle()
+                            .overlay {
+                                Text("오늘의 한마디\n다다? 블리시?")
+                                    .foregroundStyle(Color.white)
+                            }
+                    }
+                }
+            }
+            .padding(.horizontal, 60)
+            .padding(.bottom, 40)
+        }
     }
 }
 
