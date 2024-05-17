15ea703b84ddad9677635b5ec8f06031f592ff8b
Anjin Lee <anjin720jin@naver.com>
2024-05-17 14:08:32 +0900

    Feat: 화면 기능에 따라 폴더 분리

---
+++ PlanIO/Main/Input/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  PlanIO
+//
+//  Created by Anjin on 5/17/24.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
