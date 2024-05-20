309de22f2b6dafcad82d671a9ffd3cdb4c6eb01a
Anjin Lee <anjin720jin@naver.com>
2024-05-20 15:46:17 +0900

    Feat: Plan model -> Task 필요로 중단

---
+++ PlanIO/Data/Plan.swift
@@ -0,0 +1,20 @@
+//
+//  Plan.swift
+//  PlanIO
+//
+//  Created by Anjin on 5/20/24.
+//
+
+import SwiftData
+import SwiftUI
+
+//@Model
+//class Plan {
+//    let date: Date
+//    var tasks: [Task]
+//    
+//    init(date: Date, tasks: [Task]) {
+//        self.date = date
+//        self.tasks = tasks
+//    }
+//}
