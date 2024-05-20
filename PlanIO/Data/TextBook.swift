832da786abe3cde45064f46e548b25868b359d4b
Anjin Lee <anjin720jin@naver.com>
2024-05-20 15:43:19 +0900

    Feat: WorkBook model

--- PlanIO/Data/TextBook.swift
+++ PlanIO/Data/TextBook.swift
@@ -8,29 +8,32 @@
 import SwiftUI
 
 struct LargeTextBookChapter {
-    let id: Int
+    let id = UUID()
+    let chapter: Int
     let title: String
     let midChapters: [MidTextBookChapter]
 }
 
 struct MidTextBookChapter {
-    let id: Int
+    let id = UUID()
+    let chapter: Int
     let title: String
     let smallChapters: [SmallTextBookChapter]
 }
 
 struct SmallTextBookChapter {
-    let id: Int
+    let id = UUID()
+    let chapter: Int
     let title: String
     let startPage: Int
 }
 
 struct TextBook {
-    let contents: [LargeTextBookChapter] = [
-        LargeTextBookChapter(id: 1, title: "화학 반응의 규칙과 에너지 변화", midChapters: [
-            MidTextBookChapter(id: 1, title: "물질 변화와 화학 반응식", smallChapters: [
-                SmallTextBookChapter(id: 1, title: "주변에서 일어나는 물질 변화", startPage: 12),
-                SmallTextBookChapter(id: 2, title: "화학 반응을 나타내는 화학 반응식", startPage: 16)
+    static let contents: [LargeTextBookChapter] = [
+        LargeTextBookChapter(chapter: 1, title: "화학 반응의 규칙과 에너지 변화", midChapters: [
+            MidTextBookChapter(chapter: 1, title: "물질 변화와 화학 반응식", smallChapters: [
+                SmallTextBookChapter(chapter: 1, title: "주변에서 일어나는 물질 변화", startPage: 12),
+                SmallTextBookChapter(chapter: 2, title: "화학 반응을 나타내는 화학 반응식", startPage: 16)
             ])
         ])
     ]
