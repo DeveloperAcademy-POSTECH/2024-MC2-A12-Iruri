e8a7e25c731c4af447e4aca7cd510626ec257335
Anjin Lee <anjin720jin@naver.com>
2024-05-23 15:21:04 +0900

    Feat: Task를 추가하는 기능 구현

---
+++ PlanIO/Data/Task/Task.swift
@@ -0,0 +1,53 @@
+//
+//  Task.swift
+//  PlanIO
+//
+//  Created by Anjin on 5/20/24.
+//
+
+import SwiftData
+import SwiftUI
+
+@Model
+class Task: Identifiable, Equatable {
+    let id = UUID()
+    var title: String
+    var type: TaskType
+    var status: TaskStatus
+    
+    init(title: String, type: TaskType, status: TaskStatus) {
+        self.title = title
+        self.type = type
+        self.status = status
+    }
+}
+
+enum TaskType: String, Codable {
+    case concept = "개념"
+    case practice = "응용"
+    case other = "기타"
+    
+    var title: String {
+        self.rawValue
+    }
+}
+
+enum TaskStatus: String, Codable, CaseIterable {
+    case complete = "완료"
+    case inProgress = "진행중"
+    case incomplete = "미실천"
+    case none = "아직"
+    
+    var title: String {
+        self.rawValue
+    }
+    
+    var color: Color {
+        switch self {
+        case .complete: Color.planIODarkBlue
+        case .inProgress: Color.planIODarkYellow
+        case .incomplete: Color.red
+        case .none: Color.planIODarkGray
+        }
+    }
+}
---
+++ PlanIO/Data/Task/TaskManager.swift
@@ -0,0 +1,43 @@
+//
+//  TaskManager.swift
+//  PlanIO
+//
+//  Created by Anjin on 5/23/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct TaskManager {
+    static func makeTask(modelContext: ModelContext, scopes: [LargeTextBookChapter]) {
+        let workbooks: [LargeWorkBookChapter] = WorkBook.contents
+        
+        for large in scopes {
+            for mid in large.midChapters {
+                for small in mid.smallChapters {
+                    // 개념 Task 추가
+                    let conceptTask = Task(title: "\(large.chapter)-\(mid.chapter)-\(small.chapter). \(small.title)", type: .concept, status: .none)
+                    modelContext.insert(conceptTask)
+                }
+                
+                // 응용 Task 추가
+                let largeWorkBook = workbooks[large.chapter - 1]
+                let midWorkBook = largeWorkBook.midChapters[mid.chapter - 1]
+                
+                let practiceTask = Task(title: "오투 \(largeWorkBook.chapter)-\(midWorkBook.chapter). \(midWorkBook.title)", type: .practice, status: .none)
+                modelContext.insert(practiceTask)
+            }
+        }
+    }
+}
+
+extension TaskManager {
+    static let dummy3: [Task] = [
+        Task(title: "1-1-1. 자극을 전달하는 신경계", type: .concept, status: .complete),
+        Task(title: "1-1-2. 신경계를 통해 일어나는 반응", type: .concept, status: .inProgress),
+        Task(title: "1-1-3. 신경계를 통해 일어나는 반응을 뭘까용구링", type: .concept, status: .incomplete)
+    ]
+    
+    static let dummy1 = Task(title: "1-1-1. 자극을 전달하는 신경계", type: .concept, status: .complete)
+    static let dummy2 = Task(title: "1-1-2. 신경계를 통해 일어나는 반응", type: .concept, status: .inProgress)
+}
