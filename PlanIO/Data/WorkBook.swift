cb7e6c0c97b63e29c22627a21eadb9dad153c711
Anjin Lee <anjin720jin@naver.com>
2024-05-23 15:36:07 +0900

    Feat: 학기에 맞춰 문제집 목차 분리

--- PlanIO/Data/WorkBook.swift
+++ PlanIO/Data/WorkBook.swift
@@ -21,7 +21,7 @@ struct MidWorkBookChapter: Identifiable {
 }
 
 struct WorkBook {
-    static let contents: [LargeWorkBookChapter] = [
+    static let contents1: [LargeWorkBookChapter] = [
         LargeWorkBookChapter(chapter: 1, title: "화학 반응의 규칙과 에너지 변화", midChapters: [
             MidWorkBookChapter(chapter: 1, title: "물질 변화와 화학 반응식"),
             MidWorkBookChapter(chapter: 2, title: "화학 반응의 규칙"),
@@ -29,13 +29,13 @@ struct WorkBook {
         ]),
         
         LargeWorkBookChapter(chapter: 2, title: "기권과 날씨", midChapters: [
-            MidWorkBookChapter(chapter: 1, title: "기권과 지구 기온"),
-            MidWorkBookChapter(chapter: 2, title: "구름과 강수"),
+            MidWorkBookChapter(chapter: 1, title: "기권과 지구 기온"), // 교과서 1
+            MidWorkBookChapter(chapter: 2, title: "구름과 강수"), // 교과서 2
             // 교과서(3.기압과 날씨) = 문제지3-1(3.기압과 바람, 4, 날씨와 변화)
             // 각각 소단원 (1), (2,3)
-            MidWorkBookChapter(chapter: 3, title: "기압과 바람"),
+            MidWorkBookChapter(chapter: 3, title: "기압과 바람"), // 교과서 3
             // 위와 같음
-            MidWorkBookChapter(chapter: 4, title: "날씨와 변화"),
+            MidWorkBookChapter(chapter: 4, title: "날씨와 변화"), // 교과서 3
         ]),
         
         LargeWorkBookChapter(chapter: 3, title: "운동과 에너지", midChapters: [
@@ -46,20 +46,22 @@ struct WorkBook {
         LargeWorkBookChapter(chapter: 4, title: "자극과 반응", midChapters: [
             MidWorkBookChapter(chapter: 1, title: "감각 기관"),
             MidWorkBookChapter(chapter: 2, title: "신경계와 호르몬"),
-        ]),
-        
+        ])
+    ]
+    
+    static let contents2: [LargeWorkBookChapter] = [
         LargeWorkBookChapter(chapter: 1, title: "생식과유전", midChapters: [
             // 교과서(1.생식) = 문제지3-2(1.새포 분열, 2.사람의 발생)
             // 각각 소단원 (1,2,3), (4)
-            MidWorkBookChapter(chapter: 1, title: "세포 분열"),
+            MidWorkBookChapter(chapter: 1, title: "세포 분열"), // 교과서 1
             // 위와 같음
-            MidWorkBookChapter(chapter: 2, title: "사람의 발생"),
+            MidWorkBookChapter(chapter: 2, title: "사람의 발생"), // 교과서 1
             
             // 교과서(2.유전) = 문제지3-2(3.멘델의 유전 원리, 4.사람의 유전)
             // 각각 소단원 (1,2), (3,4)
-            MidWorkBookChapter(chapter: 3, title: "멘델의 유전 원리"),
+            MidWorkBookChapter(chapter: 3, title: "멘델의 유전 원리"), // 교과서 2
             // 위와 같음
-            MidWorkBookChapter(chapter: 4, title: "사람의 유전"),
+            MidWorkBookChapter(chapter: 4, title: "사람의 유전"), // 교과서 2
         ]),
         
         LargeWorkBookChapter(chapter: 2, title: "에너지 전환과 보존", midChapters: [
@@ -70,12 +72,12 @@ struct WorkBook {
         LargeWorkBookChapter(chapter: 3, title: "별과 우주", midChapters: [
             // 교과서(1.별) = 문제지3-2(1.별까지의 거리, 2.별의 성질)
             // 각각 소단원 (1), (2,3)
-            MidWorkBookChapter(chapter: 1, title: "별까지의 거리"),
+            MidWorkBookChapter(chapter: 1, title: "별까지의 거리"), // 교과서 1
             // 위와 같음
-            MidWorkBookChapter(chapter: 2, title: "별의 성질"),
+            MidWorkBookChapter(chapter: 2, title: "별의 성질"), // 교과서 1
             
             // 교과서(2.은하와 우주, 3.우주 탐사) = 문제지3-2(3.은하와 우주)
-            MidWorkBookChapter(chapter: 3, title: "은하와 우주"),
+            MidWorkBookChapter(chapter: 3, title: "은하와 우주"), // 교과서 2, 3
         ]),
         
         LargeWorkBookChapter(chapter: 4, title: "과학기술과 인류 문명", midChapters: [
