91a1c0ba0c7f17b21c059349689a4f4602c4227f
Il Seo <rephisto47@gmail.com>
2024-05-24 15:35:43 +0900

    Feat: 실천의 좌측 네비게이션(임시)

--- PlanIO/Main/Plan/PlanNavigation/PlanNavigationSplitView.swift
+++ PlanIO/Main/Plan/PlanNavigation/PlanNavigationSplitView.swift
@@ -9,21 +9,7 @@ import SwiftData
 import SwiftUI
 
 
-/**
- TODO
- 1. navigation 타이틀에 버튼 추가 가능한지 확인
- 2. + 버튼 눌렀을때, 어떤 방식을 통해 추가 될 것인지
- - 모달창을 띄워서 받기
- 3. row를 보여주는 view에서 순서를 바꿀 수 있어야 한다.
- <- 일단은 드래그 앤 드롭이랑 충돌해서 구현 할지 고민해야함.
- 4.
- 
- 
- 
- */
-
 struct PlanNavigationSplitView: View {
-//    @Environment(\.modelContext) private var modelContext
     
     var body: some View {
         VStack(){
@@ -33,43 +19,17 @@ struct PlanNavigationSplitView: View {
                 Spacer()
                 Text("+")
                     .onTapGesture {
-                        //TODO 모달창 만들기
+                        //TODO: Task 추가 기능 구현
                     }
             }
-            //            .frame(height: 40)
+            .padding(.horizontal, 10)
             
-            //TODO
-            // VStack 으로 각 테스크별 창 만들기
-            // Task를 통으로 받을지, type 별로 따로 받을지
-            // 이건 EO의 설계를 그대로 따라가는 식으로 해야한다.
+            PlanNavigationTaskListView()
             
-            // 데이터는 Task.dummy3 ㄹㄹ 가져오면 됨
-            // 이거 유사한 View와 설계를 다르게 해야한다.
-            // 이쪽은 스크롤뷰가 두개가 들어가버림
-            // 당연히 더블 스크롤 뷰는 쓰지 않는게 좋겠지
-            
-//            List{
-                
-                //            ScrollView{
-                //                VStack{
-                
-                // 개념
-                PlanNavigationTaskListView(type: .concept)
-                //
-                //                // 응용
-//                PlanNavigationTaskListView(type: .practice)
-//                //
-//                //                // 기타
-//                PlanNavigationTaskListView(type: .other)
-                
-                
-                //                }
-                //            }
-//            }
             Spacer()
             
             Button {
-                //TODO
+                // TODO: 자동배치 기능
             } label: {
                 HStack {
                     Spacer()
@@ -83,19 +43,10 @@ struct PlanNavigationSplitView: View {
                 .background(Color.planIOYellow)
                 .clipShape(RoundedRectangle(cornerRadius: 6))
                 .shadow(color: .black.opacity(0.3), radius: 1.5, x: 0, y: 1)
+                .padding(.horizontal, 10)
+                .padding(.bottom,12)
             }
-            
-            
-            
-            
-            
         }
-        .padding(.horizontal, 10)
-        .padding(.bottom,12)
-        
-        // TODO
-        //        .navigationTitle("Title")
-        
     }
 }
 
@@ -103,14 +54,14 @@ struct PlanNavigationSplitView: View {
 
 #Preview {
     let config = ModelConfiguration(isStoredInMemoryOnly: true)
-        let container = try! ModelContainer(for: Task.self, configurations: config)
-
+    let container = try! ModelContainer(for: Task.self, configurations: config)
+    
     for i in TaskManager.dummy3 {
         container.mainContext.insert(i)
     }
-
-        return PlanView().modelContainer(container)
+    
+    return PlanView().modelContainer(container)
     
     
-//    PlanView()
+    //    PlanView()
 }
--- PlanIO/Main/Plan/PlanNavigation/PlanNavigationTaskListView.swift
+++ PlanIO/Main/Plan/PlanNavigation/PlanNavigationTaskListView.swift
@@ -9,63 +9,111 @@ import SwiftUI
 import SwiftData
 
 struct PlanNavigationTaskListView: View {
-    let type: TaskType
     @Query(sort: \Task.title) var tasks: [Task]
-    @State private var isSectionExpanded: Bool = true
+    @State private var isConceptSectionExpanded: Bool = true
+    @State private var isPracticeSectionExpanded: Bool = true
+    @State private var isOtherSectionExpanded: Bool = true
+    
+    private let valueListRowInsets: [CGFloat] = [3,-5,3,-5]
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            // 소제목 부분
-            HStack{
-                Text(type.title) // 개념, 응용, 기타
-                    .font(.body).bold()
-                    .foregroundStyle(Color.planIODarkGray)
-                    .padding(.leading, 10)
+        let conceptTasks: [Task] = tasks.filter { $0.type == .concept }
+        let practiceTasks: [Task] = tasks.filter { $0.type == .practice }
+        let otherTasks: [Task] = tasks.filter { $0.type == .other }
+        let toggleup = Image(systemName: "chevron.up")
+        let toggledown = Image(systemName: "chevron.down")
+
+        
+        
+        VStack(alignment: .leading, spacing: 0) {
+            List(){
                 
-                //TODO
-                // 토글만들어라
-                Spacer()
+                // 개념
+                HStack{
+                    Text(TaskType.concept.title)
+                        .font(.body).bold()
+                        .foregroundStyle(Color.planIODarkGray)
+                        .padding(.leading, 10)
+                    
+                    Spacer()
+                    
+                    Button {
+                        isConceptSectionExpanded.toggle()
+                    }label: {
+                        isConceptSectionExpanded ? toggleup : toggledown
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: valueListRowInsets[0], leading: valueListRowInsets[1], bottom: valueListRowInsets[2], trailing: valueListRowInsets[3]))
                 
-                
-                
-                Text("^")
-                    .onTapGesture {
-                        isSectionExpanded.toggle()
-                    }// 토글 상태에 따라서 바뀌어 줘야함.
-                ///////// 여기서 상테를 변경을 하면
-            }
-            
-            if isSectionExpanded{
-                List(){
-                    ForEach(tasks.indices) { index in
-                        let task = tasks[index]
+                if isConceptSectionExpanded{
+                    ForEach(conceptTasks.indices) { index in
+                        let task = conceptTasks[index]
                         PlanNavigationTaskRow(task: task)
                     }
-                    .onDelete(perform: { indexSet in
-                        // TODO
-                    })
+                }
+                
+                
+                // 응용
+                HStack{
+                    Text(TaskType.practice.title)
+                        .font(.body).bold()
+                        .foregroundStyle(Color.planIODarkGray)
+                        .padding(.leading, 10)
+                    
+                    Spacer()
+                    
+                    Button {
+                        isPracticeSectionExpanded.toggle()
+                    }label: {
+                        isPracticeSectionExpanded ? toggleup : toggledown
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: valueListRowInsets[0], leading: valueListRowInsets[1], bottom: valueListRowInsets[2], trailing: valueListRowInsets[3]))
+                
+                if isPracticeSectionExpanded{
+                    ForEach(practiceTasks.indices) { index in
+                        let task = practiceTasks[index]
+                        PlanNavigationTaskRow(task: task)
+                    }
+                }
+                
+                
+                // 기타
+                HStack{
+                    Text(TaskType.other.title)
+                        .font(.body).bold()
+                        .foregroundStyle(Color.planIODarkGray)
+                        .padding(.leading, 10)
+                    
+                    Spacer()
+                    
+                    Button {
+                        isOtherSectionExpanded.toggle()
+                    }label: {
+                        isOtherSectionExpanded ? toggleup : toggledown
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: valueListRowInsets[0], leading: valueListRowInsets[1], bottom: valueListRowInsets[2], trailing: valueListRowInsets[3]))
+                
+                if isOtherSectionExpanded{
+                    ForEach(otherTasks.indices) { index in
+                        let task = otherTasks[index]
+                        PlanNavigationTaskRow(task: task)
+                    }
                 }
             }
-            
         }
     }
 }
-
 
 #Preview {
     let config = ModelConfiguration(isStoredInMemoryOnly: true)
     let container = try! ModelContainer(for: Task.self, configurations: config)
     
     for i in TaskManager.dummy3 {
-        //            let task = TaskManager.dummy1
-        //            let task2 = TaskManager.dummy2
         container.mainContext.insert(i)
     }
     
-    return PlanNavigationTaskListView(type: .concept).modelContainer(container)
-    
-//    return PlanView().modelContainer(container)
-    
-    
-    //    PlanView()
+    return PlanView().modelContainer(container)
+
 }
